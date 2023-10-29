02aa80cc6d8a0f40270b93464e7522f9436cde16
dblatcher <dv.bltchr@gmail.com>
2023-10-29 19:36:03 +0000

    refactor and reorder

--- src/game-state/cycle.ts
+++ src/game-state/cycle.ts
@@ -19,21 +19,14 @@ const fireCannons = (game: GameState) => {
     })
 }
 
-export const cycle = (gameState: GameState, directives: Directive[], pushLog: { (newLog: string): void }): GameState => {
-    const game = { ...gameState }
-    game.cycleNumber = game.cycleNumber + 1
-
-    const [player] = game.ships
-    if (player) {
-        followDirectives(player, directives)
-    }
-
+const handleProjectileHitsAndLandings = (game: GameState, pushLog: { (newLog: string): void }) => {
     const projectilesThatHitSomething: Projectile[] = []
     game.projectiles.forEach(projectile => {
         game.ships.forEach(ship => {
             if (willProjectileHitShip(projectile, ship)) {
                 projectilesThatHitSomething.push(projectile)
                 pushLog(`Hit ${ship.name ?? 'a ship'}`)
+                // TO DO - damage ship
                 createImpact({ ...projectile, timeLeft: 50 }, game)
             }
         })
@@ -41,10 +34,24 @@ export const cycle = (gameState: GameState, directives: Directive[], pushLog: { 
 
     const projectilesThatDidNotHitAnyThing = game.projectiles.filter(projectile => !projectilesThatHitSomething.includes(projectile))
     const [projectilesInAir, projectilesLanded] = splitArray(projectilesThatDidNotHitAnyThing, projectile => projectile.z > 0)
-    game.projectiles = projectilesInAir
     projectilesLanded.forEach(projectile => {
         createSplash({ x: projectile.x, y: projectile.y, timeLeft: 50, radius: 2 }, game)
     })
+    game.projectiles = projectilesInAir
+}
+
+export const cycle = (
+    gameState: GameState,
+    directives: Directive[],
+    pushLog: { (newLog: string): void }
+): GameState => {
+    const game = { ...gameState }
+    game.cycleNumber = game.cycleNumber + 1
+
+    const [player] = game.ships
+    if (player) {
+        followDirectives(player, directives)
+    }
 
     const collisons: Collison[] = []
     game.ships.forEach(ship => {
@@ -57,6 +64,9 @@ export const cycle = (gameState: GameState, directives: Directive[], pushLog: { 
         updateEffect(effect)
     })
     game.effects = game.effects.filter(effect => effect.timeLeft > 0)
+
+    handleProjectileHitsAndLandings(game, pushLog)
+
 
     // TO DO - handle collisions
 
