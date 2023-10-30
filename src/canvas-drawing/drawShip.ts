b3a4d88a539fc0a44c3a4ed750424d5d6757e121
dblatcher <dv.bltchr@gmail.com>
2023-10-30 12:26:44 +0000

    shape more boaty

--- src/canvas-drawing/drawShip.ts
+++ src/canvas-drawing/drawShip.ts
@@ -1,46 +1,50 @@
 import { getCollisionCircles } from "../game-state/ship";
 import { Ship } from "../game-state/types";
-import { getXYVector, translate } from "../lib/geometry";
+import { XY, _90_DEG_LEFT, _90_DEG_RIGHT, getXYVector, translate } from "../lib/geometry";
 import { OffsetDrawMethods } from "./drawWithOffSet";
 
+const s = (xy: XY): [number, number] => [xy.x, xy.y]
 
-export const drawShipBase = (ctx: CanvasRenderingContext2D, drawMethods: OffsetDrawMethods, ship: Ship) => {
-    const { x, y, h, width, length } = ship
+export const drawShipBase = (
+    ctx: CanvasRenderingContext2D,
+    drawMethods: OffsetDrawMethods,
+    ship: Ship,
+    showCollision = false
+) => {
+    const { h, width, length } = ship
     const { arc, lineTo, moveTo } = drawMethods
-    const fore = translate(getXYVector((length) / 2, h), { x, y })
-    const back = translate(getXYVector(-length / 2, h), { x, y })
-    const foreCircle = translate(getXYVector((length - width) / 2, h), { x, y })
-    const backCircle = translate(getXYVector(-(length - width) / 2, h), { x, y })
+    const fore = translate(getXYVector((length) / 2, h), ship)
+    const back = translate(getXYVector(-length / 2, h), ship)
 
-    const collisionCircles = getCollisionCircles(ship)
-
-    collisionCircles.forEach(circle => {
-        ctx.beginPath();
-        ctx.fillStyle = 'grey'
-        arc(circle.x, circle.y, circle.r, 0, Math.PI * 2)
-        ctx.fill();
-    })
-
-    ctx.beginPath();
-    ctx.strokeStyle = 'brown'
-    arc(backCircle.x, backCircle.y, width / 2, 0, Math.PI * 2)
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.strokeStyle = 'brown'
-    arc(x, y, width / 2, 0, Math.PI * 2)
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.strokeStyle = 'red'
-    arc(foreCircle.x, foreCircle.y, width / 2, 0, Math.PI * 2)
-    ctx.stroke();
+    const foreBack = translate(fore, getXYVector(width / 2, h + _90_DEG_LEFT + _90_DEG_LEFT))
+    const foreLeft = translate(foreBack, getXYVector(width / 2, h + _90_DEG_LEFT))
+    const foreRight = translate(foreBack, getXYVector(width / 2, h + _90_DEG_RIGHT))
+    const backLeft = translate(back, getXYVector(width / 2, h + _90_DEG_LEFT))
+    const backRight = translate(back, getXYVector(width / 2, h + _90_DEG_RIGHT))
 
     ctx.beginPath()
-    ctx.strokeStyle = 'black'
-    moveTo(back.x, back.y)
-    lineTo(fore.x, fore.y)
-    ctx.stroke();
+    ctx.fillStyle = 'brown'
+    ctx.strokeStyle = 'goldenrod'
+
+    moveTo(...s(fore))
+    lineTo(...s(foreLeft))
+    lineTo(...s(backLeft))
+    lineTo(...s(backRight))
+    lineTo(...s(foreRight))
+    lineTo(...s(fore))
+    ctx.stroke()
+    if (!showCollision) {
+        ctx.fill()
+    }
+    if (showCollision) {
+        const collisionCircles = getCollisionCircles(ship)
+        collisionCircles.forEach(circle => {
+            ctx.beginPath();
+            ctx.fillStyle = 'rgba(255,0,0,.5)'
+            arc(circle.x, circle.y, circle.r, 0, Math.PI * 2)
+            ctx.fill();
+        })
+    }
 }
 
 export const drawShipMast = (ctx: CanvasRenderingContext2D, drawMethods: OffsetDrawMethods, ship: Ship) => {
