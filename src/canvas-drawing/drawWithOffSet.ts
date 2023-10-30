ad4fa6ebb25bc133a233d87054eb559704d3dc97
dblatcher <dv.bltchr@gmail.com>
2023-10-30 12:59:21 +0000

    arcTo

--- src/canvas-drawing/drawWithOffSet.ts
+++ src/canvas-drawing/drawWithOffSet.ts
@@ -4,6 +4,7 @@ export type OffsetDrawMethods = {
     arc: (x: number, y: number, radius: number, startAngle: number, endAngle: number, counterclockwise?: boolean | undefined) => void;
     moveTo: (x: number, y: number) => void;
     lineTo: (x: number, y: number) => void;
+    arcTo: (x1: number, y1: number, x2: number, y2: number, radius: number) => void;
 }
 
 export const drawWithOffset = (ctx: CanvasRenderingContext2D, viewPort: ViewPort): OffsetDrawMethods => {
@@ -13,6 +14,8 @@ export const drawWithOffset = (ctx: CanvasRenderingContext2D, viewPort: ViewPort
     )
     const moveTo = (x: number, y: number) => ctx.moveTo(x - viewPort.x, y - viewPort.y)
     const lineTo = (x: number, y: number) => ctx.lineTo(x - viewPort.x, y - viewPort.y)
+    const arcTo = (x1: number, y1: number, x2: number, y2: number, radius: number) =>
+        ctx.arcTo(x1 - viewPort.x, y1 - viewPort.y, x2 - viewPort.x, y2 - viewPort.y, radius)
 
-    return { arc, moveTo, lineTo }
+    return { arc, moveTo, lineTo, arcTo }
 }
