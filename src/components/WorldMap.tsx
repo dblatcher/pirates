b6d5296f118a84667f588689f720b009afd68853
dblatcher <dv.bltchr@gmail.com>
2023-11-06 00:58:58 +0000

    plot ships on the map

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,5 +1,5 @@
 import { TERRAIN_SQUARE_SIZE } from "../game-state/land"
-import { GameState } from "../game-state/types"
+import { GameState, Ship } from "../game-state/types"
 import { CellMatrix } from "../lib/path-finding/types"
 import { CanvasScreen } from "./CanvasScreen"
 
@@ -13,7 +13,6 @@ interface Props {
 
 
 const plotMatrix = (ctx: CanvasRenderingContext2D, matrix: CellMatrix) => {
-
     for (let y = 0; y < matrix.length; y++) {
         for (let x = 0; x < matrix[y].length; x++) {
             const terrain = matrix[y][x]
@@ -29,7 +28,21 @@ const plotMatrix = (ctx: CanvasRenderingContext2D, matrix: CellMatrix) => {
     }
 }
 
-export const WorldMap = ({ closeModal, mapHeight, mapWidth, matrix }: Props) => {
+const crossSize = 20
+const plotShips = (ctx: CanvasRenderingContext2D, ships: Ship[]) => {
+    ships.forEach(ship => {
+        ctx.beginPath()
+        ctx.strokeStyle = 'white'
+        ctx.lineWidth = 5
+        ctx.moveTo(ship.x - crossSize, ship.y - crossSize)
+        ctx.lineTo(ship.x + crossSize, ship.y + crossSize)
+        ctx.moveTo(ship.x + crossSize, ship.y - crossSize)
+        ctx.lineTo(ship.x - crossSize, ship.y + crossSize)
+        ctx.stroke()
+    })
+}
+
+export const WorldMap = ({ closeModal, mapHeight, mapWidth, matrix, gameState }: Props) => {
 
     const drawMap = (canvas: HTMLCanvasElement) => {
         const ctx = canvas.getContext('2d')
@@ -39,13 +52,8 @@ export const WorldMap = ({ closeModal, mapHeight, mapWidth, matrix }: Props) => 
         ctx.beginPath()
         ctx.fillStyle = 'blue'
         ctx.fillRect(0, 0, mapWidth, mapHeight)
-
-        ctx.strokeStyle = 'red'
-        ctx.arc(0, 0, 100, 0, 6)
-        ctx.stroke()
-
         plotMatrix(ctx, matrix)
-
+        plotShips(ctx, gameState.ships)
     }
 
     return (
@@ -58,7 +66,7 @@ export const WorldMap = ({ closeModal, mapHeight, mapWidth, matrix }: Props) => 
 
                 <CanvasScreen width={mapWidth} height={mapHeight} draw={drawMap}
                     containerStyle={{
-                        width: mapWidth / 10,
+                        width: mapWidth / 5,
                         height: mapWidth / 10,
                     }}
                     canvasStyle={{
