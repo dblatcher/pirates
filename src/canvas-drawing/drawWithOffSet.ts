aa9485a6ccfc14deee0c6ccaae14e485f32454ea
dblatcher <dv.bltchr@gmail.com>
2023-11-16 22:49:32 +0000

    add drawImage

--- src/canvas-drawing/drawWithOffSet.ts
+++ src/canvas-drawing/drawWithOffSet.ts
@@ -7,6 +7,11 @@ export type OffsetDrawMethods = {
     arcTo: (x1: number, y1: number, x2: number, y2: number, radius: number) => void;
     rect: (x: number, y: number, w: number, h: number) => void;
     quadraticCurveTo: (cpx: number, cpy: number, x: number, y: number) => void
+    drawImage: (
+        image: CanvasImageSource,
+        x1: number, y1: number, w1?: number, h1?: number,
+        x2?: number, y2?: number, w2?: number, h2?: number,
+    ) => void
 }
 
 export const drawWithOffset = (ctx: CanvasRenderingContext2D, viewPort: ViewPort): OffsetDrawMethods => {
@@ -25,5 +30,20 @@ export const drawWithOffset = (ctx: CanvasRenderingContext2D, viewPort: ViewPort
     const quadraticCurveTo = (cpx: number, cpy: number, x: number, y: number) =>
         ctx.quadraticCurveTo(cpx - viewPort.x, cpy - viewPort.y, x - viewPort.x, y - viewPort.y)
 
-    return { arc, moveTo, lineTo, arcTo, rect, quadraticCurveTo }
+    const drawImage = (
+        image: CanvasImageSource,
+        x1: number, y1: number, w1?: number, h1?: number,
+        x2?: number, y2?: number, w2?: number, h2?: number,
+    ) =>
+        (
+            typeof w1 === 'number' && typeof h1 === 'number' &&
+            typeof x2 === 'number' && typeof y2 === 'number' &&
+            typeof w2 === 'number' && typeof h2 === 'number'
+        )
+            ? ctx.drawImage(image, x1, y1, w1, h1, x2 - viewPort.x, y2 - viewPort.y, w2, h2)
+            : (typeof w1 === 'number' && typeof h1 === 'number')
+                ? ctx.drawImage(image, x1 - viewPort.x, y1 - viewPort.y, w1, h1)
+                : ctx.drawImage(image, x1 - viewPort.x, y1 - viewPort.y)
+
+    return { arc, moveTo, lineTo, arcTo, rect, quadraticCurveTo, drawImage }
 }
