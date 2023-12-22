f3fa96cfa6c10fdb002d7300cd989a97c05b9f04
dblatcher <dv.bltchr@gmail.com>
2023-12-22 10:57:41 +0000

    move game logic up from Keyboard function to GameControls

--- src/components/GameControls.tsx
+++ src/components/GameControls.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Directive, FiringPattern, Order, Ship } from "../game-state"
+import { Directive, FiringPattern, Order, Ship, Side } from "../game-state"
 import { GunneryWidget } from "./GunneryWidget"
 import { KeyboardControls } from "./KeyboardControls"
 import { SailsWidget } from "./SailsWidget"
@@ -14,10 +14,54 @@ interface Props {
     wheelRef: React.MutableRefObject<number | undefined>
 }
 
+const directiveKeys: Record<string, Directive | undefined> = {
+    'KeyW': { order: Order.SAILS_BY, quantity: .1 },
+    'KeyS': { order: Order.SAILS_BY, quantity: -.1 },
+    'KeyQ': { order: Order.FIRE, side: Side.LEFT, pattern: FiringPattern.ALTERNATE },
+    'KeyE': { order: Order.FIRE, side: Side.RIGHT, pattern: FiringPattern.ALTERNATE },
+    'Space': { order: Order.INVADE_TOWN },
+}
+
+const patternKeys: Record<string, FiringPattern | undefined> = {
+    'Digit1': FiringPattern.BROADSIDE,
+    'Digit2': FiringPattern.CASCADE,
+    'Digit3': FiringPattern.ALTERNATE,
+}
+
 export const GameControls = ({ player, addDirective, paused, playerWheel, wheelRef }: Props) => {
 
     const [firingPattern, setFiringPattern] = useState<FiringPattern>(FiringPattern.BROADSIDE)
     const setWheelTo = (value: number) => { wheelRef.current = value }
+
+    const keyDownFunction = (event: KeyboardEvent) => {
+        const directive = directiveKeys[event.code]
+        if (paused) {
+            return
+        }
+        if (directive) {
+            if (directive.order === Order.FIRE) {
+                addDirective({ ...directive, pattern: firingPattern })
+            } else {
+                addDirective(directive)
+            }
+        }
+        const patternChange = patternKeys[event.code]
+        if (typeof patternChange === 'number') {
+            setFiringPattern(patternChange)
+        }
+    }
+
+    const keyMapFunction = (keyMap: Record<string, boolean>) => {
+        const turn = keyMap['KeyA']
+            ? .5
+            : keyMap['KeyD']
+                ? -.5
+                : undefined
+
+        if (turn) {
+            setWheelTo(turn)
+        }
+    }
 
     return (
         <aside style={{ display: 'flex' }}>
@@ -43,11 +87,9 @@ export const GameControls = ({ player, addDirective, paused, playerWheel, wheelR
                     ship={player}
                 />
                 <KeyboardControls
-                    addDirective={addDirective}
-                    firingPattern={firingPattern}
-                    setFiringPattern={setFiringPattern}
-                    setWheelTo={setWheelTo}
-                    paused={paused} />
+                    keyDownFunction={keyDownFunction}
+                    keyMapFunction={keyMapFunction}
+                />
             </>
             )}
         </aside>
--- src/components/KeyboardControls.tsx
+++ src/components/KeyboardControls.tsx
@@ -1,48 +1,19 @@
 import { useEffect, useState } from "react"
-import { Directive, Order, Side, FiringPattern } from "../game-state"
 import { useInterval } from "../hooks/useInterval"
 
 interface Props {
-    paused: boolean
-    addDirective: { (directive: Directive): void }
-    firingPattern: FiringPattern,
-    setFiringPattern: { (firingPattern: FiringPattern): void }
-    setWheelTo: { (value: number): void }
+    keyDownFunction?: { (event: KeyboardEvent): void }
+    keyMapFunction?: { (keyMap: Record<string, boolean>): void }
 }
 
-const directiveKeys: Record<string, Directive | undefined> = {
-    'KeyW': { order: Order.SAILS_BY, quantity: .1 },
-    'KeyS': { order: Order.SAILS_BY, quantity: -.1 },
-    'KeyQ': { order: Order.FIRE, side: Side.LEFT, pattern: FiringPattern.ALTERNATE },
-    'KeyE': { order: Order.FIRE, side: Side.RIGHT, pattern: FiringPattern.ALTERNATE },
-    'Space': { order: Order.INVADE_TOWN },
-}
 
-const patternKeys: Record<string, FiringPattern | undefined> = {
-    'Digit1': FiringPattern.BROADSIDE,
-    'Digit2': FiringPattern.CASCADE,
-    'Digit3': FiringPattern.ALTERNATE,
-}
-
-export const KeyboardControls = ({ paused, addDirective, firingPattern, setFiringPattern, setWheelTo }: Props) => {
+export const KeyboardControls = ({ keyMapFunction, keyDownFunction }: Props) => {
     const [keyMap, setKeyMap] = useState<Record<string, boolean>>({})
 
     const handleKeyDown = (event: KeyboardEvent) => {
         setKeyMap({ ...keyMap, [event.code]: true })
-        if (paused) {
-            return
-        }
-        const directive = directiveKeys[event.code]
-        if (directive) {
-            if (directive.order === Order.FIRE) {
-                addDirective({ ...directive, pattern: firingPattern })
-            } else {
-                addDirective(directive)
-            }
-        }
-        const patternChange = patternKeys[event.code]
-        if (typeof patternChange === 'number') {
-            setFiringPattern(patternChange)
+        if (keyDownFunction) {
+            keyDownFunction(event)
         }
     }
 
@@ -61,16 +32,10 @@ export const KeyboardControls = ({ paused, addDirective, firingPattern, setFirin
 
 
     useInterval(() => {
-        const turn = keyMap['KeyA']
-            ? .5
-            : keyMap['KeyD']
-                ? -.5
-                : undefined
-
-        if (turn) {
-            setWheelTo(turn)
+        if (keyMapFunction) {
+            keyMapFunction(keyMap)
         }
-    }, 0)
+    }, keyDownFunction ? 0 : null)
 
     return <></>
 }
