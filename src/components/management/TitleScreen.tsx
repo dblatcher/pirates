823f1cf3e34f01dc0068281ba2c806bf77f0b7f6
dblatcher <dv.bltchr@gmail.com>
2024-01-07 16:33:52 +0000

    add a silly paper image border

--- src/components/management/TitleScreen.tsx
+++ src/components/management/TitleScreen.tsx
@@ -8,15 +8,17 @@ type Props = {
 export const TitleScreen = ({ setScenario, scenarios }: Props) => {
 
     return (
-        <main className="dialog title-screen">
-            <h1>Buccaneer</h1>
-            <div className="button-stack">
-                {Object.entries(scenarios).map(([key, scenario]) => (
-                    <button key={key} onClick={() => {
-                        setScenario(scenario)
-                    }}>{scenario.name ?? key}</button>
-                ))}
-            </div>
-        </main>
+        <div className="title-frame paper">
+            <main className="title-screen skull-stamp">
+                <h1>Buccaneer</h1>
+                <div className="button-stack">
+                    {Object.entries(scenarios).map(([key, scenario]) => (
+                        <button key={key} onClick={() => {
+                            setScenario(scenario)
+                        }}>{scenario.name ?? key}</button>
+                    ))}
+                </div>
+            </main>
+        </div>
     )
 }
