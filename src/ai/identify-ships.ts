8090bcd42768dde3f472dc65ef303d6d42874760
dblatcher <dv.bltchr@gmail.com>
2024-01-13 10:04:22 +0000

    refine the range check in identifyShips

--- src/ai/identify-ships.ts
+++ src/ai/identify-ships.ts
@@ -1,7 +1,6 @@
 import type { GameState, Town, Ship } from "../game-state"
-import { Rect, isPointInsideRect } from "../lib/geometry"
+import { Rect, getDistance, isPointInsideRect } from "../lib/geometry"
 import { splitArray } from "../lib/util"
-
 
 export const identifyShips = (observer: Ship | Town, gameState: GameState, sightRange: number) => {
     const sightRect: Rect = {
@@ -10,7 +9,9 @@ export const identifyShips = (observer: Ship | Town, gameState: GameState, sight
         bottom: observer.y + sightRange,
         right: observer.x + sightRange,
     }
-    const nearbyShips = gameState.ships.filter(otherShip => observer !== otherShip && isPointInsideRect(otherShip, sightRect))
+    const nearbyShips = gameState.ships
+        .filter(otherShip => observer !== otherShip && isPointInsideRect(otherShip, sightRect))
+        .filter(otherShip => getDistance(otherShip, observer) <= sightRange)
     const [enemies, allies] = splitArray(nearbyShips, (otherShip) => otherShip.faction !== observer.faction)
     return { enemies, allies }
 }
