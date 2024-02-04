3891621fb45d265de224b417cfbc69a372c68bf6
dblatcher <dv.bltchr@gmail.com>
2024-02-04 10:18:10 +0000

    fix targetting bug

--- src/scenarios/logic-check.ts
+++ src/scenarios/logic-check.ts
@@ -1,9 +1,13 @@
 import { GAME_STATE_DEFAULTS, Scenario } from ".";
+import { MissonAi } from "../ai/mission-ai";
 import { TerrainType, inputToLandmass } from "../game-state/land";
 import { GameState } from "../game-state/model";
 import { makeDefaultShip } from "../game-state/ship";
 import { makeTownWithForts } from "../game-state/towns";
 import { _DEG, xy } from "../lib/geometry";
+
+makeTownWithForts;
+xy;
 
 const makeInitialState = (): GameState => {
 
@@ -17,21 +21,21 @@ const makeInitialState = (): GameState => {
         },
         ships: [
             makeDefaultShip({
-                x: 550, y: 300, h: _DEG * 90, id: 2, faction: 'grance'
+                x: 550, y: 300, h: _DEG * 90, id: 2, faction: 'grance',name:'grance-A'
             }),
             makeDefaultShip({
                 x: 550, y: 350, h: _DEG * -90, id: 3, faction: 'spaim',
                 sailLevel: .1, sailLevelTarget: .1,
             }),
-            // makeDefaultShip({
-            //     x: 550, y: 400, h: _DEG * 93, id: 4, faction: 'spaim',
-            //     ai: new MissonAi({
-            //         mission: {
-            //             type: 'travel',
-            //         },
-            //         path: [],
-            //     }, 4, true)
-            // }),
+            makeDefaultShip({
+                x: 550, y: 400, h: _DEG * 93, id: 4, faction: 'spaim',
+                ai: new MissonAi({
+                    mission: {
+                        type: 'travel',
+                    },
+                    path: [],
+                }, 4, true)
+            }),
             // makeDefaultShip({
             //     x: 750, y: 300, h: _DEG * 90, id: 5, faction: 'grance'
             // }),
@@ -61,15 +65,15 @@ const makeInitialState = (): GameState => {
             }
         ].map(inputToLandmass),
         towns: [
-            makeTownWithForts({
-                x: 560, y: 550,
-                faction: 'spaim',
-                id: 1,
-                name: 'test town',
+            // makeTownWithForts({
+            //     x: 560, y: 550,
+            //     faction: 'spaim',
+            //     id: 1,
+            //     name: 'test town',
 
-            }, [
-                xy(0, -100)
-            ])
+            // }, [
+            //     xy(0, -100)
+            // ])
         ],
     }
 
