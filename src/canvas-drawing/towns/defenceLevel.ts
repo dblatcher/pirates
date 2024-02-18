e6775e21f8e05ec1aa520a6aa1e4a91b9dd5c583
dblatcher <dv.bltchr@gmail.com>
2024-02-18 14:32:27 +0000

    graphically show damage to defences

--- src/canvas-drawing/towns/defenceLevel.ts
+++ src/canvas-drawing/towns/defenceLevel.ts
@@ -1,21 +1,12 @@
-import { DEFENCES_TO_REPEL_INVADERS, TOWN_SIZE, Town } from "../../game-state";
+import { TOWN_SIZE, Town } from "../../game-state";
 import { OffsetDrawMethods } from "../drawWithOffSet";
-import { flash } from "../helpers";
 
 export const showDefenceLevel = (
     ctx: CanvasRenderingContext2D,
     drawingMethods: OffsetDrawMethods,
     town: Town,
-    cycleNumber: number,
 ) => {
     const { fillText } = drawingMethods
-
-    ctx.beginPath()
-    const defenceString = `${town.defences}/${town.profile.maxDefences}`
-    ctx.font = '20px arial'
-    ctx.fillStyle = town.defences < DEFENCES_TO_REPEL_INVADERS ? flash(cycleNumber, ['red', 'white']) : 'white'
-    fillText(defenceString, town.x - TOWN_SIZE / 2, town.y)
-
     ctx.beginPath()
     const garrisonString = `${town.garrison} troops`
     ctx.font = '18px arial'
--- src/canvas-drawing/towns/outline.ts
+++ src/canvas-drawing/towns/outline.ts
@@ -1,20 +1,77 @@
-import { TOWN_SIZE, Town } from "../../game-state";
+import { DEFENCES_TO_REPEL_INVADERS, TOWN_SIZE, Town } from "../../game-state";
+import { clamp } from "../../lib/util";
 import { OffsetDrawMethods } from "../drawWithOffSet";
 import { flash } from "../helpers";
+
+
+const POINTS_PER_WALL = 50
+const WALL_WIDTH = 4
+
+const getWallOffset = (damageLevel: number, isOuterWall: boolean) => {
+    const clampedDamageLevel = clamp(damageLevel)
+    const minimumStroke = (isOuterWall ? 0 : 8);
+    return [minimumStroke + 24 * (1 - clampedDamageLevel), clampedDamageLevel * 24]
+}
+
+const drawWall = (
+    ctx: CanvasRenderingContext2D,
+    drawingMethods: OffsetDrawMethods,
+    town: Town,
+
+    wallWidth: number,
+    damageLevel: number,
+    offset: number,
+    isOuterWall: boolean,
+) => {
+
+    ctx.beginPath();
+    ctx.lineWidth = wallWidth;
+    ctx.setLineDash(getWallOffset(damageLevel, isOuterWall))
+    drawingMethods.arc(town.x, town.y, (TOWN_SIZE / 2) + offset, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.setLineDash([])
+
+}
+
+const dividePointsByWall = (totalWalls: number, defences: number): number[] => {
+    const walls: number[] = []
+    let pointsLeft = defences
+    for (let i = 0; i < totalWalls; i++) {
+        if (pointsLeft <= 0) {
+            walls.push(0)
+            continue
+        }
+        const amountForNextWall = Math.min(POINTS_PER_WALL, pointsLeft)
+        walls.push(amountForNextWall)
+        pointsLeft = pointsLeft - amountForNextWall
+    }
+    return walls
+}
 
 export const drawTownOutline = (
     ctx: CanvasRenderingContext2D,
     drawingMethods: OffsetDrawMethods,
     town: Town,
     cycleNumber: number,
-    beingInvaded: boolean,
 ) => {
-
     ctx.beginPath();
-    ctx.lineWidth = 8;
-    ctx.strokeStyle = beingInvaded ? flash(cycleNumber, ['red', 'black'], 15) : 'black'
     ctx.fillStyle = 'gray'
     drawingMethods.arc(town.x, town.y, TOWN_SIZE / 2, 0, Math.PI * 2);
-    ctx.stroke();
     ctx.fill();
+
+    const townCanBeInvaded = town.defences < DEFENCES_TO_REPEL_INVADERS
+    const totalWalls = Math.ceil(town.profile.maxDefences / POINTS_PER_WALL)
+
+    ctx.strokeStyle = townCanBeInvaded ? flash(cycleNumber, ['red', 'black']) : 'black'
+    // inner wall first
+    dividePointsByWall(totalWalls, town.defences).forEach((amountOfPoints, index) => {
+        drawWall(
+            ctx, drawingMethods, town,
+            WALL_WIDTH,
+            1 - (amountOfPoints / POINTS_PER_WALL),
+            (index - totalWalls) * WALL_WIDTH,
+            index > 0
+        )
+    })
+
 }
