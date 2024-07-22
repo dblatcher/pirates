d2df9d28bd63c0dfd86b4fcace80827f55ca0d75
dblatcher <dv.bltchr@gmail.com>
2024-07-22 21:32:40 +0100

    do away with draw icon and the repair icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,5 @@
 import coastlinesPng from "./coastlines.png"
 import StrawHatIconPng from "./Straw_hat_icon.svg.png"
-import repairPng from "./repair.png"
 import captainHatPng from "./captain-hat.png"
 import housePng from "./house.png"
 import treesPng from "./trees.png"
@@ -16,9 +15,7 @@ type AssetData = {
 
 const coastlines: AssetData = {
     width: 200, height: 200, src: coastlinesPng,
-
 }
-const REPAIR: AssetData = { width: 100, height: 100, src: repairPng }
 const HOUSE: AssetData = { width: 100, height: 100, src: housePng }
 const TREES: AssetData = {
     width: 60, height: 80, src: treesPng, sprites: {
@@ -34,10 +31,9 @@ const MISC: AssetData = {
 
 const assetParams = {
     coastlines,
-    REPAIR,
     HOUSE,
     TREES,
     MISC,
 } satisfies Record<string, AssetData>
 
-export { assetParams, coastlinesPng, StrawHatIconPng, repairPng, captainHatPng, housePng }
+export { assetParams, coastlinesPng, StrawHatIconPng, captainHatPng, housePng }
--- src/canvas-drawing/ships/drawShips.ts
+++ src/canvas-drawing/ships/drawShips.ts
@@ -1,7 +1,8 @@
+import { AssetMap } from "../../context/asset-context"
 import { GameState, ViewPort } from "../../game-state"
 import { isShipInView } from "../../game-state/ship"
 import { timePhase } from "../../lib/util"
-import { drawIcon } from "../draw-icon"
+import { drawSpriteFunc } from "../draw-sprite"
 import { OffsetDrawMethods } from "../drawWithOffSet"
 import { drawShipBase } from "./base"
 import { drawDamage } from "./damage"
@@ -11,12 +12,14 @@ import { drawShipMasts } from "./masts"
 export const drawShips = (
     ctx: CanvasRenderingContext2D,
     drawingMethods: OffsetDrawMethods,
+    assets: AssetMap,
     viewPort: ViewPort,
     gameState: GameState,
     showCollision = false
 ) => {
 
     const { ships, wind, cycleNumber } = gameState
+    const drawSprite = drawSpriteFunc(drawingMethods, assets)
     const shipsInView = ships.filter(ship => isShipInView(ship, viewPort))
     shipsInView.forEach(ship => drawShipBase(ctx, drawingMethods, ship, showCollision))
     shipsInView.forEach(ship => drawDamage(ctx, drawingMethods, ship, cycleNumber))
@@ -26,7 +29,7 @@ export const drawShips = (
     shipsInView.forEach(ship => {
         if (ship.underRepair) {
             const phase = timePhase(cycleNumber, 30, 1)
-            drawIcon(ctx, drawingMethods, ship, { icon: 'REPAIR', width: 100 + phase, height: 100 + phase })
+            drawSprite({ key: 'MISC', width: 50 + phase, height: 50 + phase, x: ship.x, y: ship.y, center: true, fx: 2, fy: 2 })
         }
     })
 }
